da3cb4161bc9f794062dbd5f3724e330e19e0857
aghimanshu123 <97390553+aghimanshu123@users.noreply.github.com>
2022-11-11 08:44:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const mongoose=require("mongoose");
+mongoose.connect(
+    "mongodb+srv://Himanshu123:user-123@cluster0.1dcajdh.mongodb.net/User",
+    {useNewUrlParser:true},
+    (err) => {
+     if(err) console.log(err) 
+     else console.log("mongdb is connected");
+    }
+  );
+const app=express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+const personSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+});
+var Person=mongoose.model("Person",personSchema);
+app.post('/login',function(req,res){
+    var name = req.body.name;
+	var pass = req.body.password;
+    Person.findOne({name:name,pass:pass},function(err,details){
+        if(err){
+            return res.sendFile(__dirname +"/public/failure.html");
+        }
+        if(!details){
+            return res.sendFile(__dirname +"/public/failure.html"); 
+        }
+        return res.sendFile(__dirname +"/public/index2.html");
+    
+    })
+});
+
+app.post('/sign_up', async function(req,res){
+	var name = req.body.name;
+	var email =req.body.email;
+	var pass = req.body.password;
+
+    const person=new Person({
+    name:name,
+    email:email,
+    password:pass,
+});
+const saveddata=await person.save();
+res.sendFile(__dirname +"/public/index2.html");
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+  })
+
+
+app.get('/',function(req,res){
+res.set({
+	'Access-control-Allow-Origin': '*'
+	});
+return res.redirect('index.html');
+}).listen(3000)
+
+
+console.log("server listening at port 3000");
+
